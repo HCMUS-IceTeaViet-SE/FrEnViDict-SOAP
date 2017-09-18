03d44238a3d28bda04be95d145b17de85d8a4e37
nhoxbypass <nhoxbypass@gmail.com>
2017-09-18 23:51:49 +0700

    implement services and UI for testing

--- FVDict/Properties/Settings.Designer.cs
+++ FVDict/Properties/Settings.Designer.cs
@@ -8,22 +8,38 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FVDict.Properties
-{
-
-
+namespace FVDict.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.WebServiceUrl)]
+        [global::System.Configuration.DefaultSettingValueAttribute("http://localhost:2270/EnViTranslationService.asmx")]
+        public string FVDict_EnViTranslationServiceProxy_EnViTranslationService {
+            get {
+                return ((string)(this["FVDict_EnViTranslationServiceProxy_EnViTranslationService"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.WebServiceUrl)]
+        [global::System.Configuration.DefaultSettingValueAttribute("http://localhost:2270/FrEnTranslationService.asmx")]
+        public string FVDict_FrEnTranslationServiceProxy_FrEnTranslationService {
+            get {
+                return ((string)(this["FVDict_FrEnTranslationServiceProxy_FrEnTranslationService"]));
             }
         }
     }
